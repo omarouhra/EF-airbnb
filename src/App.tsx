5ab5ee49b35c2d7cd45c769cdb0411d5cbabe288
Ilias Haddad <iliasshaddad18@gmail.com>
2023-07-25 12:04:57 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Player } from "@editframe/react";
+import { useState } from "react";
 
 function App() {
+  const [playerState, setPlayerState] =  useState<"stopped" | "playing" | "paused">("playing"); // you can use this to control the player
+  const [seek, setSeek] = useState(0); // you can use this to seek the player
+  const config = {
+    backgroundColor: "#000000FF",
+    dimensions: {
+      height: 1080,
+      width: 1920,
+    },
+    duration: 10,
+    metadata: {},
+    layers: [],
+  }; // this is the config for the player to render the video 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <Player
+          config={config}
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+          applicationId="demo"
+          loop={false}
+          playerState={playerState}
+          seek={seek}
+        />
+      </div>
     </div>
   );
 }
-
 export default App;
