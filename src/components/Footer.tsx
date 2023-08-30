9b66de29917e68394f16f60ae7b13f7907ab8ce3
Omar Ouhra <omarmaroc26@gmail.com>
2023-08-30 19:51:03 +0100

    fix: footer dark mode style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-center py-12 border-t border-gray-100">
+    <div className="text-center py-12 border-t border-gray-100 dark:border-black/60">
       <p className="text-sm">
         Built using{" "}
         <a
