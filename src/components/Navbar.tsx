2abc8bc6b71b22c531ae427f84cad08d618bab6e
Omar Ouhra <omarmaroc26@gmail.com>
2023-08-30 19:50:19 +0100

    feat: navbar themes button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
 import EditframeIcom from "../icons/EditframeIcom";
 
+import { useTheme } from "next-themes";
+import MoonIcon from "../icons/MoonIcon";
+import SunIcon from "../icons/SunIcon";
+
 const Navbar = () => {
+  const { resolvedTheme, theme, setTheme } = useTheme();
+
   return (
-    <div className="flex items-center space-x-4">
-      <EditframeIcom /> <p className="text-lg font-semibold">EditFrame</p>
+    <div className="flex items-center justify-between space-x-4">
+      <div className="flex items-center space-x-2">
+        <EditframeIcom /> <p className="text-lg font-semibold">EditFrame</p>
+      </div>
+      <button
+        onClick={() => {
+          setTheme(resolvedTheme === "light" ? "dark" : "light");
+        }}
+        type="button"
+        className="rounded-md p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
+      >
+        {theme === "light" ? <MoonIcon /> : <SunIcon />}
+      </button>
     </div>
   );
 };
